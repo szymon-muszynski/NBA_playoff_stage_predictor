01dc6fd82c3d8507432508b006ae10ad43ace57d
szymon-muszynski <s.muszynski2003@gmail.com>
2026-06-13 16:26:32 +0200

    Added implementation to get_home_team_win_chance function

--- app/backend/simulation.py
+++ app/backend/simulation.py
@@ -1,18 +1,41 @@
 import random
+import pandas as pd
 
-def get_home_team_win_chance(home_team, away_team):
-    pass
+def get_home_team_win_chance(home_team: str, away_team: str, season: str, df_stats:pd.DataFrame, models_dict: dict) -> float:
+    season_artifacts = models_dict.get(season)
+    if not season_artifacts:
+        raise ValueError(f"Brak modelu dla sezonu {season}")
+    
+    model = season_artifacts['model']
+    scaler = season_artifacts['scaler']
+    features = season_artifacts['features']
+    
+    stats_season = df_stats[df_stats['SEASON_YEAR'] == season]
+    home_stats = stats_season[stats_season['TEAM_ABBREVIATION'] == home_team].iloc[0]
+    away_stats = stats_season[stats_season['TEAM_ABBREVIATION'] == away_team].iloc[0]
 
-def simulate_single_match(home_team_win_chance):
+    delta_plus_minus = home_stats['AVG_PLUS_MINUS'] - away_stats['AVG_PLUS_MINUS']
+    delta_win_pct = home_stats['WIN_PCT'] - away_stats['WIN_PCT']
+    delta_home_away_win_pct = home_stats['HOME_WIN_PCT'] - away_stats['AWAY_WIN_PCT']
+
+    X_input = pd.DataFrame([[delta_plus_minus, delta_win_pct, delta_home_away_win_pct]], columns=features)
+    X_scaled = scaler.transform(X_input)
+    prob = model.predict_proba(X_scaled)
+    
+    home_team_win_chance = prob[0][1]
+    
+    return home_team_win_chance
+
+def simulate_single_match(home_team_win_chance: float) -> bool:
     return random.random() < home_team_win_chance
 
 
-def simulate_single_series(home_team, away_team):
+def simulate_single_series(home_team, away_team, season):
     
     home_team_score = 0
     away_team_score = 0
     
-    home_team_win_chance = get_home_team_win_chance(home_team = home_team,away_team=away_team)
+    home_team_win_chance = get_home_team_win_chance(home_team, away_team, season)
     
     while True:
         if simulate_single_match(home_team_win_chance):
